01134cb914672762386e4343c43e71983f39e04a
zwm <3501160898@qq.com>
2026-08-14 17:29:31 +0800

    更新webgl

---
+++ Web/serve.py
@@ -0,0 +1,30 @@
+import os
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+os.chdir(ROOT)
+
+class H(SimpleHTTPRequestHandler):
+    def guess_type(self, path):
+        path = path.split("?")[0]
+        if path.endswith(".js.gz") or path.endswith(".js"):
+            return "application/javascript"
+        if path.endswith(".wasm.gz") or path.endswith(".wasm"):
+            return "application/wasm"
+        if path.endswith(".data.gz") or path.endswith(".data"):
+            return "application/octet-stream"
+        return super().guess_type(path)
+
+    def end_headers(self):
+        p = self.path.split("?")[0]
+        if p.endswith(".gz"):
+            self.send_header("Content-Encoding", "gzip")
+        if p.endswith(".br"):
+            self.send_header("Content-Encoding", "br")
+        super().end_headers()
+
+if __name__ == "__main__":
+    print("目录:", ROOT)
+    print("index.html:", os.path.exists(os.path.join(ROOT, "index.html")))
+    print("打开这个: http://127.0.0.1:8765/")
+    ThreadingHTTPServer(("127.0.0.1", 8765), H).serve_forever()
